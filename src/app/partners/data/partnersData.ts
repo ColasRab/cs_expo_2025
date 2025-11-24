b2aa523cc222854ccfac91f58deeb86a3de23971
Rab Karl Colasino <colasinorabkarl@gmail.com>
2025-11-24 12:43:46 +0800

    feat: added partners

--- src/app/partners/data/partnersData.ts
+++ src/app/partners/data/partnersData.ts
@@ -8,197 +8,293 @@ export interface Partner {
 export const allPartners: Partner[] = [
   // Diamond Sponsors
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Le'rese",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Le'rese.png",
+    link: "https://www.facebook.com/share/1BYrMP9Kvs/",
     tier: "Diamond Sponsors"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "GitHub",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Github.png",
+    link: "https://github.com/",
     tier: "Diamond Sponsors"
   },
   // Gold Sponsors
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Capabara",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Capabara.png",
+    link: "https://www.capabara.com/",
     tier: "Gold Sponsors"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Kooapps Philippines",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Kooapps.PNG",
+    link: "https://kooapps.com/",
+    tier: "Gold Sponsors"
+  },
+  {
+    name: "Mim Krafts",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Mimi.png",
+    link: "http://Facebook.com/mim.krafts",
+    tier: "Gold Sponsors"
+  },
+  {
+    name: "Better Blend",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/BetterBlend.png",
+    link: "https://www.facebook.com/betterblendph/",
+    tier: "Gold Sponsors"
+  },
+  {
+    name: "The Photobox",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Photobox.png",
+    link: "https://www.facebook.com/share/1AT9LMLU7q/?mibextid=wwXIfr",
     tier: "Gold Sponsors"
   },
   // Silver Sponsors
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "It’s A Wrap Shawarma",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Wrap.png",
+    link: "https://www.facebook.com/itsawrapp",
     tier: "Silver Sponsors"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Genos' Food Hub",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Genos.png",
+    link: "https://www.facebook.com/GenosPastaHub",
+    tier: "Silver Sponsors"
+  },
+  {
+    name: "Abiva Publishing House, Inc.",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Abiva.png",
+    link: "https://abiva.com.ph/",
+    tier: "Silver Sponsors"
+  },
+  {
+    name: "Zus Coffee",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Zus.png",
+    link: "https://zuscoffee.ph/",
     tier: "Silver Sponsors"
   },
   // Media Partners
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Philippine News Agency (PNA)",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/PNA.png",
+    link: "https://www.pna.gov.ph/",
     tier: "Media Partners"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "IKOT.PH",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Ikotph.png",
+    link: "https://www.facebook.com/IKOTPH/",
+    tier: "Media Partners"
+  },
+  {
+    name: "WhenInManila.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/When.png",
+    link: "http://WhenInManila.com",
+    tier: "Media Partners"
+  },
+  {
+    name: "GREAT MINDS MEDIA, INC.",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Gadgets.png",
+    link: "https://www.facebook.com/GadgetsMagazine",
+    tier: "Media Partners"
+  },
+  {
+    name: "INQUIRER POP!",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Inquirer.png",
+    link: "http://INQUIRER.net",
+    tier: "Media Partners"
+  },
+  {
+    name: "BRABO News Network",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Brabo.png",
+    link: "https://brabonewsph.com/",
     tier: "Media Partners"
   },
   // Bronze Sponsors
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Tiny Treasures Club",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/TTC.PNG",
+    link: "https://www.facebook.com/profile.php?id=61556139826508&mibextid=wwXIfr&mibextid=wwXIfr",
     tier: "Bronze Sponsors"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "SILOBOWL",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Silobowl.png",
+    link: "https://www.facebook.com/silobowl",
     tier: "Bronze Sponsors"
   },
   // Minor Partners
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "PUP Association of Students for Computer Intelligence and Integration",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/PUP-ASCII.png",
+    link: "#",
     tier: "Minor Partners"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "UST Society of Information Technology Enthusiasts",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/SITE.png",
+    link: "#",
+    tier: "Minor Partners"
+  },
+  {
+    name: "RTU Junior Marketing Association - Boni Campus",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/RTU_JMA.jpg",
+    link: "#",
+    tier: "Minor Partners"
+  },
+  {
+    name: "The PNU Math Club",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/tpmc.jpg",
+    link: "#",
+    tier: "Minor Partners"
+  },
+  {
+    name: "AdU-SC of International Society for Pharmaceutical Engineering",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/AdU-SC_ISPE.jpg",
+    link: "#",
+    tier: "Minor Partners"
+  },
+  {
+    name: "Academic Consortium of Computer Science Students",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/ACCeSS.png",
+    link: "#",
+    tier: "Minor Partners"
+  },
+  {
+    name: "Alliance of Computer Science Students UPLB",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/ACSS_UPLB.png",
+    link: "#",
     tier: "Minor Partners"
   },
   // Major Partners
   {
     name: "PUP The Programmers' Guild",
-    logo: "/partners/placeholder/logo1.png",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/PUP_TPG.png",
     link: "https://www.facebook.com/PUPTPG",
     tier: "Major Partners"
   },
   {
     name: "Asia Pacific College – Microsoft Community",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/MSC.png",
+    link: "https://www.facebook.com/APCMSC/",
     tier: "Major Partners"
   },
   {
     name: "IEEP - Cebu Technological University Student Chapter",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/MSC.png",
+    link: "#",
     tier: "Major Partners"
   },
   {
     name: "AWS Learning Club – UST",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/AWSLC-UST.png",
+    link: "https://www.facebook.com/awsust",
     tier: "Major Partners"
   },
   {
     name: "UST Computer Science Society",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/CSS.png",
+    link: "https://www.facebook.com/USTCSS",
     tier: "Major Partners"
   },
   {
     name: "Google Developer Groups on Campus – Pamantasan ng Lungsod ng Maynila",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/GDG_PLM.png",
+    link: "#",
     tier: "Major Partners"
   },
   {
     name: "Computer Engineering Organization – FEU Institute of Technology",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/CpEO_FEU.png",
+    link: "https://www.facebook.com/feutechCpEO",
     tier: "Major Partners"
   },
   {
     name: "iTamaraws Esports Club – FEU Institute of Technology",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/TEC.png",
+    link: "https://www.facebook.com/iTamarawsEsportsClub",
     tier: "Major Partners"
   },
   {
     name: "ACM Student Chapter  – FEU Institute of Technology",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/FIT_ACM.png",
+    link: "https://www.facebook.com/feutechACM",
     tier: "Major Partners"
   },
   {
     name: "ACM Student Chapter – FEU Alabang",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/FEUA_ACM.png",
+    link: "https://www.facebook.com/profile.php/?id=61574931573106",
     tier: "Major Partners"
   },
   {
     name: "PLM Society of Information Technology Enthusiasts",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/PLM_SITE.png",
+    link: "https://www.facebook.com/plmsite/",
     tier: "Major Partners"
   },
   {
     name: "Google Developer Groups on Campus - Loyola",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/GDG_Loyola.png",
+    link: "https://www.facebook.com/gdgloyola/",
     tier: "Major Partners"
   },
   {
     name: "Junior Philippine Computer Society - APC",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/JPCS-APC.svg",
+    link: "https://www.facebook.com/JPCSAPC",
     tier: "Major Partners"
   },
   {
     name: "Computer Science Student Association",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/COMSA.png",
+    link: "https://www.facebook.com/tatakComsa",
     tier: "Major Partners"
   },
   {
-    name: " Xavier Computer Enthusiasts’ League",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Xavier Computer Enthusiasts’ League",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/XCEL.png",
+    link: "https://www.facebook.com/XU.XCEL",
     tier: "Major Partners"
   },
   {
     name: "Junior Information Systems Security Association",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/JISSA.png",
+    link: "https://www.facebook.com/jissa.tipqc/",
     tier: "Major Partners"
   },
   {
     name: "University of Santo Tomas Technovation Society",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/UST_TechSoc.png",
+    link: "https://www.facebook.com/technovsoc/",
     tier: "Major Partners"
   },
   
   // Community Development Partners
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Filipino Web Development Peers",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/FWDP.png",
+    link: "https://www.facebook.com/fwdpeers/",
     tier: "Community Development Partners"
   },
   {
-    name: "Partner Name",
-    logo: "/partners/placeholder/logo1.png",
-    link: "https://example.com",
+    name: "Data Engineering Pilipinas",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/Data.png",
+    link: "https://dataengineering.ph/",
+    tier: "Community Development Partners"
+  },
+  {
+    name: "KadaKareer",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/KadaKareer.png",
+    link: "https://kadakareer.carrd.co/",
+    tier: "Community Development Partners"
+  },
+  {
+    name: "Devbits",
+    logo: "https://oyjvxrlsiaithbqhjgtl.supabase.co/storage/v1/object/public/Misc/partners/devbits.png",
+    link: "https://devbits.ph/",
     tier: "Community Development Partners"
   },
 ];
